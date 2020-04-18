4ab6b14153f0cbf51909827c73ff30f9c6d1aa9a
kanaparthi <kanaparthi@kanaparthis-mbp.dlink>
2020-04-18 17:30:56 +0530

    second commit

---
+++ servlet_2/src/main/java/com/example/servlet/RegisterLeague.java
@@ -0,0 +1,51 @@
+package com.example.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterLeague
+ */
+public class RegisterLeague extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doProcess(request, response);
+	}
+
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String l_name= request.getParameter("l_title");
+		String s_year = request.getParameter("l_year");
+		int year = Integer.parseInt(s_year);
+		String season = request.getParameter("l_seasons");
+		if(l_name.length()>7 && year>2020 )
+		{
+			RequestDispatcher dis = request.getRequestDispatcher("success.html");
+			dis.forward(request, response);
+			
+		}
+		else
+		{
+			RequestDispatcher dis = request.getRequestDispatcher("fail.html");
+			dis.forward(request, response);
+		}
+		
+	}
+}
